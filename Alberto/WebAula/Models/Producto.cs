3290ad0f0d1406d8835d8d509da79e2a2c8f98bb
Alberto Santamaria <alberto.santamaria@animaeducacao.com.br>
2023-05-12 12:10:26 -0300

    Add banco de dados e models

--- Alberto/WebAula/Models/Producto.cs
+++ Alberto/WebAula/Models/Producto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,40 @@ namespace WebAula.Models
 {
     public class Producto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Valor { get; set; }
+
     }
+
+    public static List<Producto> Todos()
+    {
+        List<Producto> producto = new List<Producto>();
+        /* SQL Server (SqlConnection connection = new SqlConnection(connectionString)) */
+        using (NpgsqlConnection connection = new NpgsqlConnection(ConnectionString)) /* Postgresql */
+
+        {
+            string query = "SELECT Id, Nome, Valor FROM Productos";
+            NpgsqlCommand command = new NpgsqlCommand( query, connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Producto producto = new Producto
+                {
+                    Id = (int)reader["Id"],
+                    Nome = reader["Nome"].ToString(),
+                    Valor = reader["Valor"].ToString()
+                };
+
+                if (reader["id_producto"] != DBNull.Value)
+                    cliente.Valor = new Cidade() { Id = Convert.ToInt32(reader["id_cidade"]) };
+
+                productos.Add(producto);
+            }
+        }
+        return productos;
+    }
+
 }
